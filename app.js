314a87478d88b8c3060f56ebb8f3702493564dec
aviralumrao <aviral0419@gmail.com>
2026-08-26 20:24:26 +0530

    adding text on image - issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+
 function draw(meme) {
-  const c = document.getElementById("myCanvas");
-  const ctx = c.getContext("2d");
   const img = document.getElementById(meme);
   ctx.drawImage(img, 10, 10, 400, 400);
 }
@@ -9,13 +10,16 @@ let toptxt = document.getElementById("top-text");
 
 toptxt.addEventListener("input", () => {
   console.log(toptxt.value);
+  ctx.font = "20px Georgia white";
+  ctx.textAlign = "center";
+  ctx.fillText(toptxt.value, 200, 100);
 });
 
 let bottomtxt = document.getElementById("bottom-text");
 
 bottomtxt.addEventListener("input", () => {
   console.log(bottomtxt.value);
+  ctx.font = "20px Georgia white";
+  ctx.textAlign = "center";
+  ctx.fillText(bottomtxt.value, 200, 300);
 });
-
-
-

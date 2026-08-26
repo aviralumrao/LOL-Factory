cad87031da5389e10687a7c8022e6b53f9ab05b5
aviralumrao <aviral0419@gmail.com>
2026-08-26 18:10:17 +0530

    image displays on canvas

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ function draw(meme) {
   const c = document.getElementById("myCanvas");
   const ctx = c.getContext("2d");
   const img = document.getElementById(meme);
-  ctx.drawImage(img, 10, 10);
+  ctx.drawImage(img, 10, 10, 400, 400);
 }

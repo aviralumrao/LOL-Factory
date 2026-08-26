38e00a5fceb4faebe5796519ee5a96f9300da644
aviralumrao <aviral0419@gmail.com>
2026-08-26 17:43:50 +0530

    drawing the image as per id

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function draw() {
+function draw(meme) {
   const c = document.getElementById("myCanvas");
   const ctx = c.getContext("2d");
-  const img = document.getElementById("spiderman");
+  const img = document.getElementById(meme);
   ctx.drawImage(img, 10, 10);
 }

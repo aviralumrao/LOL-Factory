ee07b3ea085696784b5720832aa8ff3675e4acd1
aviralumrao <aviral0419@gmail.com>
2026-08-26 19:14:40 +0530

    takes input using forms and read it using event listener

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ function draw(meme) {
   const img = document.getElementById(meme);
   ctx.drawImage(img, 10, 10, 400, 400);
 }
+
+let toptxt = document.getElementById("top-text");
+
+toptxt.addEventListener("input", () => {
+  console.log(toptxt.value);
+});
+
+let bottomtxt = document.getElementById("bottom-text");
+
+bottomtxt.addEventListener("input", () => {
+  console.log(bottomtxt.value);
+});
+
+
+
